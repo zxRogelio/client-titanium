991027094fffe8bb2ea5a636d8a12b2dbc5d6b55
zxRogelio <20230004@uthh.edu.mx>
2025-12-02 22:13:04 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // 👇 O quita base por completo, pero NO uses /titanium/ ni nada raro
-  base: '/',
 })
+
