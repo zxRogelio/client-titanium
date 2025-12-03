60639ce52877d937c1ccf29129576224ea6a5a34
zxRogelio <20230004@uthh.edu.mx>
2025-12-02 22:01:06 -0600

    Change Vite base path to root
    Removed base path configuration for Vite.

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/titanium/',
   plugins: [react()],
+  // 👇 O quita base por completo, pero NO uses /titanium/ ni nada raro
+  base: '/',
 })
-
-
